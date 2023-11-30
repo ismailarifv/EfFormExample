d513f88ead90580185bd4420ca43b8e162d5fa51
İsmail Arif Güleç <i.arifgulec8110@gmail.com>
2023-11-30 11:59:42 +0300

    Add olayları bitti

---
+++ EfFormExample/Entity/UserCrud.cs
@@ -0,0 +1,51 @@
+﻿using EfFormExample.Entity.Interfaces;
+using EfFormExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfFormExample.Entity
+{
+    public class UserCrud : ICrud<User>
+    {
+        DataContext db = new DataContext();
+        public bool Add(User entity)
+        {
+            if (entity != null
+                && !String.IsNullOrWhiteSpace(entity.Name)
+                && !String.IsNullOrWhiteSpace(entity.Surname)
+                && !String.IsNullOrWhiteSpace(entity.Email)
+                && !String.IsNullOrWhiteSpace(entity.Password)
+                
+                )
+            {
+                db.User.Add(entity);
+                db.SaveChanges();
+                return true;
+            }
+            return false;
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(User entity, int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- EfFormExample/Form1.cs
+++ EfFormExample/Form1.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using EfFormExample.Entity;
+using EfFormExample.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +15,97 @@ namespace EfFormExample
 {
     public partial class Form1 : Form
     {
+        RoleCrud roleCrud = new RoleCrud();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            addRoleMessage_lbl.Visible = false;
+        }
+
+        private void AddRole_btn_Click(object sender, EventArgs e)
+        {
+            Role role = new Role();
+
+            role.Name = roleName_txt.Text;
+            role.IsStatus = status_chck.Checked;
+            string RoleImage = "";
+            string imagePath = @"C:\Users\iarif\OneDrive\Masaüstü\imgs\";
+            string defaultImage = "emptyRole.png";
+            if(role_img.Image != null)
+            {
+                
+                string imageName = Guid.NewGuid() + ".jpg";
+                role_img.Image.Save(imagePath + imageName, ImageFormat.Jpeg);
+                RoleImage = imagePath + imageName;
+            }
+            else
+            {
+                RoleImage = imagePath + defaultImage;
+            }
+            role.Image = RoleImage;
+            addRoleMessage_lbl.Text = roleCrud.Add(role) ? "Role Ekleme Başarılı" : "Role Ekleme Başarısız";
+            roleName_txt.Clear();
+            status_chck.Checked = false;
+            role_img.Image = null;
+            
+        }
+
+        private void SelectImage_btn_Click(object sender, EventArgs e)
+        {
+            UploadImageBox(role_img);
+        }
+
+        private void userimg_btn_Click(object sender, EventArgs e)
+        {
+            UploadImageBox(UserImg);
+        }
+
+        public void UploadImageBox(PictureBox picture)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+               picture.ImageLocation= openFileDialog.FileName;
+            }
+        }
+
+        private void UserAdd_btn_Click(object sender, EventArgs e)
+        {
+            User user = new User();
+            user.Name = userName_txt.Text;
+            user.Surname = userSurname_txt.Text;
+            user.Email= userEmail_txt.Text;
+            user.Password = userPassword_txt.Text;
+            user.IsStatus = userStatus_chbx.Checked;
+            user.RoleId =Convert.ToInt32(userRole_cmbx.SelectedValue);
+
+            string UserImage = "";
+            string imagePath = @"C:\Users\iarif\OneDrive\Masaüstü\imgs\";
+            string defaultImage = "emptyRole.png";
+            if (UserImg.Image != null)
+            {
+
+                string imageName = Guid.NewGuid() + ".jpg";
+                UserImg.Image.Save(imagePath + imageName, ImageFormat.Jpeg);
+                UserImage = imagePath + imageName;
+            }
+            else
+            {
+                UserImage = imagePath + defaultImage;
+            }
+            user.Image = UserImage;
+            UserAddMessage_lbl.Text = userCrud.Add(user) ? "User Ekleme Başarılı" : "User Ekleme Başarısız";
+            userName_txt.Clear();
+            userSurname_txt.Clear();
+            userEmail_txt.Clear();
+            userPassword_txt.Clear();
+            userStatus_chbx.Checked=false;
+            UserImg = null;
+           
         }
     }
 }
